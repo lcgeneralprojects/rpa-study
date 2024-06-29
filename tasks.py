89576574fe5c1ac38f72bae86fa1718e77bc767a
lcgeneralprojects <lcgeneralprojects@gmail.com>
2024-06-30 02:48:08 +0500

    A few chapters done. Finishing for tonight.

---
+++ tasks.py
@@ -0,0 +1,20 @@
+from robocorp.tasks import task
+from robocorp import browser
+
+@task
+def robot_spare_bin_python():
+    """Insert the sales data for the week and export it as a PDF"""
+    browser.configure(
+        slowmo=1000,
+    )
+    open_the_intranet_website()
+    log_in()
+    
+    
+def open_the_intranet_website():
+    """Navigates to the given URL"""
+    browser.goto("https://robotsparebinindustries.com/")
+    
+    
+def log_in():
+    """Fills in the login form and clicks the 'Log in' button"""
